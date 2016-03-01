341fad2e37084f6d585a41c342635c43f662811d
Вадим <vcaregorodcev@gmail.com>
2016-03-01 23:08:19 +0300

    add starterkit

---
+++ app/index.js
@@ -0,0 +1,9 @@
+import angular from 'angular';
+
+import components from './components';
+import services from './services';
+
+angular.module('app', [
+    components,
+    services
+]);
---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+var webpack = require('webpack');
+var path = require('path');
+
+module.exports = {
+    context: path.join(__dirname, './app'),
+    entry: './index.js',
+    output: {
+        path: path.join(__dirname, './app/build'),
+        filename: 'bundle.js'
+    },
+    devtool: 'inline-source-map',
+    watch: true,
+    resolve: {
+        extensions: ['', '.js']
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                loader: 'babel',
+                exclude: /node_modules/,
+                query: {
+                    cacheDirectory: true,
+                    presets: ['es2015']
+                }
+            },
+            {
+                test: /\.jade$/,
+                loader: 'ng-cache?prefix=[dir]/[dir]!jade-html'
+            },
+            {
+                test: /\.styl$/,
+                loader: 'css!cssnext!stylus'
+            },
+            {
+                test: /\.(png|jpg|gif|svg|eot|ttf|woff)/,
+                loader: 'url'
+            }
+        ]
+    }
+};
